556cea53876973bc801817bda751ec656c54ab4a
Corwin <corwin@CorwindeMacBook-Air.local>
2026-07-15 16:44:40 +0800

    chore: restore blobfish desktop pet source

---
+++ src/preload.js
@@ -0,0 +1,13 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('petAPI', {
+  onDirection: (callback) => ipcRenderer.on('direction', (_event, dir) => callback(dir)),
+  onReminder: (callback) => ipcRenderer.on('reminder', (_event, text) => callback(text)),
+  onBump: (callback) => ipcRenderer.on('bump', () => callback()),
+  onCheckHover: (callback) => ipcRenderer.on('check-hover', (_event, x, y) => callback(x, y)),
+  setPaused: (value) => ipcRenderer.send('pause', value),
+  setIgnoreMouse: (ignore) => ipcRenderer.send('set-ignore-mouse', ignore),
+  dragStart: () => ipcRenderer.send('drag-start'),
+  dragMove: (dx, dy) => ipcRenderer.send('drag-move', dx, dy),
+  dragEnd: (vx, vy) => ipcRenderer.send('drag-end', vx, vy),
+});
